136754ba39caccc26d98b0a492c2245a88aef586
Harman <harmanjaggs@gmail.com>
2022-10-01 21:06:37 +0530

    just add appconfig

---
+++ lib/config/app_config.dart
@@ -0,0 +1,8 @@
+/// [AppConfig] App config class
+class AppConfig {
+  /// app data
+  static String apiBaseUrl = 'https://lazy-engineer.herokuapp.com/';
+  static String signUp = 'auth/signUp';
+  static String signIn = 'auth/signIn';
+  static String signOut = '/auth/signOut';
+}
--- lib/network/dio_client.dart
+++ lib/network/dio_client.dart
@@ -2,23 +2,22 @@ import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:lazy_engineer/model/user_dto/user_dto.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import '../config/app_config.dart';
 import '../model/signin_model/signin_model.dart';
 import '../model/signup_model/signup_model.dart';
 import 'dio_exception.dart';
 
-const apiBaseUrl = 'https://lazy-engineer.herokuapp.com';
-
 class DioClient {
   late final Dio _dio;
   DioClient() {
-    _dio = Dio(BaseOptions(baseUrl: apiBaseUrl));
+    _dio = Dio(BaseOptions(baseUrl: AppConfig.apiBaseUrl));
     // _dio.interceptors.add(AuthorizationInterceptor());
     _dio.interceptors.add(PrettyDioLogger());
   }
 
   Future<UserDto?> signUp(SignUpModel user) async {
     try {
-      final response = await _dio.post('/auth/signUp', data: user.toJson());
+      final response = await _dio.post(AppConfig.signUp, data: user.toJson());
       UserDto userResponse = UserDto.fromJson(response.data);
       return userResponse;
     } on DioError catch (err) {
@@ -31,7 +30,7 @@ class DioClient {
 
   Future<String?> signIn(SignInModel user) async {
     try {
-      final response = await _dio.post('/auth/signIn', data: user.toJson());
+      final response = await _dio.post(AppConfig.signIn, data: user.toJson());
       UserDto userResponse = UserDto.fromJson(response.data);
       return userResponse.data.token;
     } on DioError catch (err) {
