f000abb7f06cfd1c700c6c881a08326aade2548c
Harman <harmanjaggs@gmail.com>
2022-10-02 21:29:29 +0530

    removed a library

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 import 'my_app.dart';
 
 void main() async {
   ///remove the leading hash (#) from the URL of your Flutter web app,
+  setPathUrlStrategy();
   runApp(const MyApp());
 }
