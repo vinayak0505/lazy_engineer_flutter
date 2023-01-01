7726613d670c73cc0200e9b6951035dc3afb89ea
Harshit <harshitarora886@gmail.com>
2023-01-01 22:30:01 +0530

    Edited: Dropdown Menus in Download Screen

--- lib/features/download/pages/download_screen.dart
+++ lib/features/download/pages/download_screen.dart
@@ -5,7 +5,7 @@ import 'package:lazy_engineer/features/components/custom_text_field.dart';
 
 class DownloadScreen extends StatelessWidget {
   const DownloadScreen({Key? key}) : super(key: key);
-
+  final _value = "-1";
   @override
   Widget build(BuildContext context) {
     TextEditingController searchDownloadController = TextEditingController();
@@ -24,12 +24,110 @@ class DownloadScreen extends StatelessWidget {
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Column(children: [
-              CustomTextField(
-                controller: searchDownloadController,
-                suffixIcon: AppIcons.searchIcon,
-              ),
-            ],),
+            child: Column(
+              children: [
+                CustomTextField(
+                  controller: searchDownloadController,
+                  suffixIcon: AppIcons.searchIcon,
+                ),
+                const SizedBox(height: 16),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: DropdownButtonFormField<String>(
+                    elevation: 0,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    value: _value,
+                    onChanged: (String? value) {},
+                    items: const [
+                      DropdownMenuItem(
+                        value: '-1',
+                        child: Text('Notes'),
+                      ),
+                      DropdownMenuItem(
+                        value: '1',
+                        child: Text('Computer Graphics & Multimedia'),
+                      ),
+                      DropdownMenuItem(
+                        value: '2',
+                        child: Text('Computer Graphics & Multimedia'),
+                      ),
+                      DropdownMenuItem(
+                        value: '3',
+                        child: Text('Computer Graphics & Multimedia'),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: DropdownButtonFormField<String>(
+                    elevation: 0,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    value: _value,
+                    onChanged: (String? value) {},
+                    items: const [
+                      DropdownMenuItem(
+                        value: '-1',
+                        child: Text('Practical File'),
+                      ),
+                      DropdownMenuItem(
+                        value: '1',
+                        child: Text('Computer Graphics & Multimedia'),
+                      ),
+                      DropdownMenuItem(
+                        value: '2',
+                        child: Text('Computer Graphics & Multimedia'),
+                      ),
+                      DropdownMenuItem(
+                        value: '3',
+                        child: Text('Computer Graphics & Multimedia'),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: DropdownButtonFormField<String>(
+                    elevation: 0,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    value: _value,
+                    onChanged: (String? value) {},
+                    items: const [
+                      DropdownMenuItem(
+                        value: '-1',
+                        child: Text('Books'),
+                      ),
+                      DropdownMenuItem(
+                        value: '1',
+                        child: Text('Computer Graphics & Multimedia'),
+                      ),
+                      DropdownMenuItem(
+                        value: '2',
+                        child: Text('Computer Graphics & Multimedia'),
+                      ),
+                      DropdownMenuItem(
+                        value: '3',
+                        child: Text('Computer Graphics & Multimedia'),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
